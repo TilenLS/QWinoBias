a225a113c4d8caad278a8df33c6aa2dbf0a6a3c9
Tilen LS <tilen.stokin@gmail.com>
2024-10-01 12:42:53 +0100

    update data split

--- data_processing.py
+++ data_processing.py
@@ -48,10 +48,11 @@ def to_df(lines: [str]) -> pd.DataFrame:
 
 if __name__ == '__main__':
     os.chdir(os.getcwd() + '/wino/data')
-    paths = glob.glob("*.test")
+    paths = glob.glob("*.dev")
     lines = []
     for path in paths:
         lines += get_lines(path)
     df = to_df(lines)
+    df = df.sample(frac=1).reset_index(drop=True)
     print(df)
-    df.to_pickle('test_data.pkl')
+    df.to_pickle('train_sentences.pkl')
--- train_model.py
+++ train_model.py
@@ -1,14 +1,16 @@
-import random 
+import random, pickle, datetime 
 import pandas as pd
+from lambeq import NumpyModel, Dataset, QuantumTrainer, SPSAOptimizer, BinaryCrossEntropyLoss
+import numpy as np
 
 SEED = random.randint(0, 400)
 BATCH_SIZE = 15
 EPOCHS = 200
 
-printf("===========(MODEL SUMMARY)===========")
+print("===========(MODEL SUMMARY)===========")
 print(f"Training for [{EPOCHS}] epochs with a batch size of [{BATCH_SIZE}] and initialise with seed [{SEED}]")
 print(f"Using a numpy model with cross entropy loss and RMSE for accuracy")
-printf("=====================================")
+print("=====================================")
 
 
 def load_pkl(path: str):
@@ -17,28 +19,28 @@ def load_pkl(path: str):
     file.close()
     return data
 
-train_data = load_pkl("wino/data/data_final/train_data_2024-09-24_121915.pkl")
-validation_data = load_pkl("wino/data/data_final/train_data_2024-09-24_121915.pkl")
-test_data = load_pkl("wino/data/data_final/train_data_2024-09-24_121915.pkl")
+train_data = load_pkl("wino/data/data_final/train_data.pkl")
+validation_data = load_pkl("wino/data/data_final/val_data.pkl")
+test_data = load_pkl("wino/data/data_final/test_data.pkl")
 
-train_circuits, train_labels, train_diagrams = zip(*train_data)
-validation_circuits, validation_labels, validation_diagrams = zip(*validation_data)
-test_circuits, test_labels, test_diagrams = zip(*test_data)
+train_circuits, train_labels, train_diagrams, _ = zip(*train_data)
+validation_circuits, validation_labels, validation_diagrams, _ = zip(*validation_data)
+test_circuits, test_labels, test_diagrams, _ = zip(*test_data)
 
-total_len = len(train_labels) + len(val_labels) + len(test_labels)
-printf("===========(DATA SUMMARY)===========")
+total_len = len(train_labels) + len(validation_labels) + len(test_labels)
+print("===========(DATA SUMMARY)===========")
 print(f"Training size: {len(train_labels)} ({len(train_labels)/total_len})")
-print(f"Validation size: {len(val_labels)} ({len(val_labels)/total_len})")
+print(f"Validation size: {len(validation_labels)} ({len(validation_labels)/total_len})")
 print(f"Test size: {len(test_labels)} ({len(test_labels)/total_len})")
-printf("=====================================")
+print("=====================================")
 
-model = NumpyModel.from_diagrams(train_circuits + val_circuits + test_circuits, use_jit=False)
+model = NumpyModel.from_diagrams(train_circuits + validation_circuits + test_circuits, use_jit=False)
 
 loss = BinaryCrossEntropyLoss(use_jax=True) 
 acc = lambda y_hat, y: np.sqrt(np.mean((np.array(y_hat)-np.array(y))**2)/2)
 
 train_dataset = Dataset(train_circuits, train_labels, batch_size=BATCH_SIZE)
-val_dataset = Dataset(val_circuits, val_labels, shuffle=True)
+validation_dataset = Dataset(validation_circuits, validation_labels, shuffle=True)
 test_dataset = Dataset(test_circuits, test_labels)
 
 trainer = QuantumTrainer(model,
@@ -53,6 +55,6 @@ trainer = QuantumTrainer(model,
 
 
 print("Learning parameters: "+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S"))
-trainer.fit(train_dataset, val_dataset, eval_interval=1, log_interval=1)
+trainer.fit(train_dataset, validation_dataset, eval_interval=1, log_interval=1)
 test_acc = acc(model(test_dataset.data), test_dataset.targets)
 print('Test accuracy:', test_acc)
